c6ffa284e39f3a5a3c6127993a971ea85386d5f1
SoMZeY <vvbordia@gmail.com>
2025-08-10 22:01:35 -0400

    Started refactoring the light component to accomadate the full functionality of the phong lighting system. Will have to implement ubos as well for this.

--- include/lightComponent.h
+++ include/lightComponent.h
@@ -2,6 +2,35 @@
 #define LIGHT_COMPONENT_H
 
 #include <glm/glm.hpp>
+#include <vector>
+
+enum LightcasterType
+{
+	TYPE_UNKNOWN = -1,
+	DIRECTIONAL_LIGHT,
+	POINT_LIGHT,
+	FLASH_LIGHT,
+};
+
+struct LightValues
+{
+	// General light
+	glm::vec3 lightDir; // Can represent position based on homogeneous values
+
+	// Phong Light values
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+
+	// Attenuation specific
+	float constant;
+	float linear;
+	float quadratic;
+
+	// Flashlight specific
+	float innerCone;
+	float outerCone;
+};
 
 class PhongLightComponent
 {
@@ -15,6 +44,7 @@ public:
 private:
 	glm::vec3 color;
 	float intensity;
+	LightValues lightValues;
 
 	// TODO; refactor later with the transform object
 	glm::vec3 position;
